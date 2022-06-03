a30dd17bd8f28b33f3ce6263d27067efcd44a0a4
Rama Reksotinoyo <63135711+RamaReksotinoyo@users.noreply.github.com>
2022-06-03 15:33:25 +0700

    Update main.py

--- Machine Learning/main.py
+++ Machine Learning/main.py
@@ -22,25 +22,14 @@ data_augmentation = augmentation(train_generator)
 mnet2 = get_mobilenet(train_generator)
 model = tongkolNet_MobilenetV2(data_augmentation, mnet2)
 
-model.fit(
-    train_generator, 
-    steps_per_epoch = train_generator.samples // train_generator.batch_size,
-    validation_data = val_generator,
-    validation_steps = val_generator.samples // val_generator.batch_size,
-    epochs = 1,
-)
 
+if __name__ == '__main__':
+    model.fit(
+        train_generator, 
+        steps_per_epoch = train_generator.samples // train_generator.batch_size,
+        validation_data = val_generator,
+        validation_steps = val_generator.samples // val_generator.batch_size,
+        epochs = 1,
+    )
 
-def tflite_posttraining():
-
-    export_dir = 'Saved Model/'
-    tf.saved_model.save(model, export_dir)
-
-    converter = tf.lite.TFLiteConverter.from_saved_model(export_dir)
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    tflite_quant_model = converter.convert()
-
-    tflite_model_file = pathlib.Path('Saved Model/foo_quant2.tflite')
-    tflite_model_file.write_bytes(tflite_quant_model)
-
-tflite_posttraining()
+    model.save('foo.h5')
